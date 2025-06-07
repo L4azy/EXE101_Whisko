0cdff26eae8859e6a112d16eb4f5dac5d5eafa4f
L4azy <hungnqse183854@fpt.edu.vn>
2025-06-08 02:58:22 +0700

    Init project

---
+++ Whisko.Server/Program.cs
@@ -0,0 +1,46 @@
+// Add services to the container.
+using Newtonsoft.Json;
+
+namespace Whisko.Server
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllers()
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.ReferenceLoopHandling =
+                        ReferenceLoopHandling.Ignore;
+                });
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+
+        }
+
+    }
+}
